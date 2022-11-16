78b6d8ea6898b1c5e050be734fdc89f249ea70dd
EnMedina <medina9304@gmail.com>
2022-11-16 16:02:18 -0500

    Creating our models

---
+++ spring5webapp/src/main/java/com/enriquemedina/spring5web/Spring5webapp.java
@@ -0,0 +1,14 @@
+package com.enriquemedina.spring5web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webapp.class, args);
+
+	}
+
+}
